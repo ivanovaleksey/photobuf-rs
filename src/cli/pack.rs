87787729c6f2035cc99c397666e73e1726dfd700
Aleksey Ivanov <ialexxei@gmail.com>
2018-06-25 12:23:16 +0300

    Do not unwrap in cli::pack

--- src/cli/pack.rs
+++ src/cli/pack.rs
@@ -7,12 +7,12 @@ use std::path::PathBuf;
 pub fn call(src: PathBuf, dst: PathBuf) -> Result<()> {
     let mut hash: HashMap<u16, Vec<String>> = HashMap::new();
 
-    let entries = fs::read_dir(&src).unwrap();
+    let entries = fs::read_dir(&src)?;
     for entry in entries {
         let entry = entry?;
         let name = entry.file_name();
-        let name = name.into_string().unwrap();
-        let year: u16 = name[..4].parse().unwrap();
+        let name = name.into_string().map_err(|_| err_msg("Cannot convert OsString into String"))?;
+        let year: u16 = name[..4].parse()?;
 
         hash.entry(year)
             .and_modify(|list| list.push(name.clone()))
@@ -20,19 +20,19 @@ pub fn call(src: PathBuf, dst: PathBuf) -> Result<()> {
     }
 
     if dst.exists() {
-        fs::remove_dir_all(&dst).unwrap();
+        fs::remove_dir_all(&dst)?;
     }
 
     for (year, names) in &hash {
         let year = year.to_string();
-        fs::create_dir_all(dst.join(&year)).unwrap();
+        fs::create_dir_all(dst.join(&year))?;
 
         for name in names.iter() {
             let from = src.join(name);
             let to = dst.join(&year).join(name);
 
             debug!("Copy from: {:?}; to: {:?}", from, to);
-            fs::copy(from, to).unwrap();
+            fs::copy(from, to)?;
         }
     }
 
